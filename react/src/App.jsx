220dcff4c75627c03fc4c8dc570e112009a49eb2
Nikhitha <nikhitha.gurunatham@terralogic.com>
2026-09-20 09:39:14 +0530

    Day 1 frontend

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,44 @@
-import ProjectCard from "../src/projectCard.jsx";
+import ProjectCard from "./projectCard.jsx";
 
 function App() {
-  const project = {
-    name: "Website Redesign",
-    client: "ABC Technologies",
-    status: "In Progress",
-    owner: "Rahul",
-    hours: 120,
-    finalCost: 530332
-  };
+  const projects = [
+    {
+      id:1,
+      name: "Frontend Development",
+      client: "TCS",
+      status: "In Progress",
+      owner: "Nikhitha",
+      hours: 120,
+      finalCost: 530332
+    },
+    {
+      id:2,
+      name: "backend Development",
+      client: "ttd",
+      status: "Completed",
+      owner: "Vishwa",
+      hours: 80,
+      finalCost: 250000
+    },
+    {
+      id:3,
+      name: "E-commerce",
+      client: "amazon",
+      status: "Pending",
+      owner: "Virat",
+      hours: 60,
+      finalCost: 0
+    }
+  ];
 
   return (
     <div>
-      <ProjectCard project={project} />
+      {projects.map((project) => (
+        <ProjectCard
+    key={project.id}
+    project={project}
+  />
+      ))}
     </div>
   );
 }
